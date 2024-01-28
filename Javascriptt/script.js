101562fc7e7a04bd2b6790c3a7ae96dc8e9bd590
Nurane Ismayilzade <nuraneai@code.edu.az>
2024-01-28 06:56:33 +0400

    first commit

---
+++ Javascriptt/script.js
@@ -0,0 +1,204 @@
+"use strict";
+// let isMarried = true;
+// console.log(isMarried)
+// var surname = "Huseynov";
+// var surname ="test";
+// console.log(surname);
+// //let address="test";
+// let address = "Ehmedli";
+// console.log(address);
+// const age = 27;
+// console.log(age);
+//-----------------------------------------------------------------------------------
+// let isMarried = true;
+// isMarried=100;
+// console.log(isMarried);
+//const price= 100;
+// let price = 100;
+// price+=50;
+// console.log(price);
+//----------------------------------------------------
+// let stu = {
+//     name:"Semed",
+//     surname:"Huseynov",
+//     age:27
+// };
+// let st2 = {
+//     name:"sasasa",
+//     surname:"aaaa",
+//     age:27
+// };
+//-----------------------------------------------------------
+// stu=st2;
+// console.log(stu.name + "-" + surname);
+
+//stu = null;
+
+// stu.name="Test";
+
+// console.log(stu.name + "-" + surname)
+
+
+// let name = "XXX";
+// let surname = "YYY";
+
+//let fullName= name+"-"+surname;
+// let fullName = ${name} ${surname};
+
+// console.log(fullName);
+
+// let ages = [22,44,55,10,8,"salam"];
+// console.log(ages[ages.length-1]);
+
+// console.log(100+5);
+
+
+// let num1=100;
+// let num2=30;
+
+// let result= num1+num2;
+// console.log(result);
+
+// let num3 = 5**3;
+// console.log(num3);
+
+// let num3 = 5;
+
+// num3++;
+// console.log(num3);
+
+//let num1 = "Salam";
+// let num1 = "55";
+
+// let num2 = 55;
+
+// let result = num2 - num1;
+// console.log(num1 === num2);
+
+// let number = 100;
+ 
+// if (number > 90 && number < 120 ){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// let result = number == 100 ? "yes" : "no";
+// console.log(result);
+
+
+// let weekDay = 3;
+ 
+// switch (weekDay) {
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//        break;
+//     default:
+//         console.log("Not selected");
+//         break;
+// }
+
+// let isMarried = false;
+// if(isMarried){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// let nums = [5,4,3,2,"30"];
+
+// if(nums[nums.length-1]==30){
+//     console.log(nums[nums.length-1])
+// }else{
+//    console.log("not equal");
+// }
+
+// let nums = [1,3,5,6,7,8];
+
+// for (let i = 0; i < nums.length; i++) {
+
+//     var sum = 100;
+//     sum+=i;
+// }
+
+// console.log(sum);
+//------------------------------------------------------------
+//--------------yazdiqlarimiza uygun praktika---------------------------------------------
+// console.log("Hello Nurane");
+// let a=10;
+// let b=true;
+// let c="hello"
+// let d=12.2
+// a=false
+// console.log(c);
+// var k=12
+// var k=false
+// console.log(k);
+// let name="nurane"
+// let surname="ismayilzade"
+// let fullName=name+" "+surname
+// console.log(fullName);
+// let numbers=[1,2,3,4,5]
+// console.log(numbers);
+// console.log(numbers[numbers.length-1]);
+// console.log(numbers[9]);
+// let items=[1,"nurane",2.5,"ismayilzade",299]
+// console.log(items[2]);
+// for (let i = 0; i < items.length; i++) {
+//     console.log(items[i]);    
+// }
+// console.log(10/0);
+// console.log("nurane"-1);
+// console.log("55"-55);
+// console.log("55"-1);
+// let namea="nuranee"
+// console.log(typeof(namea));
+// console.log(1+2+4+"4"+4);
+// console.log(12==12);
+// console.log(12==="12");
+// console.log(12=="12");
+// console.log(12>13 && 14<12);
+// console.log(12!=="12");
+// let numberss=[1,2,4,6,8,9]
+// if (numberss[2]%2==0) {
+//     console.log("Eded cutdur");
+// }
+// else{
+//     console.log("Eded tekdir");
+// }
+// let day=3
+// switch (day) {
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     default:
+//         console.log("other days");
+//         break;
+// }
+// let student={
+//     name:"Nurane",
+//     surname:"Ismayilzade",
+//     age:21
+// }
+// console.log(student.name);
+// console.log(student.surname);
+// console.log(student.age);
+
+// const arr=[1,2,4,8,9];
+// let cem=0;
+// for (let i = 0; i < arr.length; i++) {
+//      cem += arr[i];  
+// }
+// console.log(cem);
+// //----------------------------------------
+// for (let i = 0; i < arr.length; i++) {
+//     cem += arr[i]; 
+//     let say=0 
+// }
+// console.log(say);
